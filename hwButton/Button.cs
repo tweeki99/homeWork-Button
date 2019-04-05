35e6c07f4a2303cb07179b20e03a031cc74c912a
tweeki99 <danil.shipilov.99@gmail.com>
2019-04-05 20:21:42 +0600

    hw-Button

---
+++ hwButton/Button.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace hwButton
+{
+    public delegate void ButtonEventHandler();
+
+    public class Button
+    {
+        public event ButtonEventHandler Click;
+        
+        public int Lenght { get; set; }
+        public int Width { get; set; }
+        public int Left { get; set; }
+        public int Top { get; set; }
+        
+        public Button(int lenght, int width, int left, int top)
+        {
+            Click += OnClick;
+
+            Lenght = lenght;
+            Width = width;
+            Left = left;
+            Top = top;
+
+            ShowButton('*');
+        }
+
+        public void ReadButton()
+        {
+            while (true)
+            {
+                var dfg = Console.ReadKey();
+                if (dfg.KeyChar == '\r')
+                Click?.Invoke();
+            }
+        }
+
+        public void OnClick()
+        {
+            ShowButton('#');
+            Thread.Sleep(500);
+            ShowButton('*');
+        }
+
+        public void ShowButton(char symbol)
+        {
+            for (int j = 0; j < Width; j++)
+            {
+                Console.SetCursorPosition(Left, Top + j);
+                for (int i = 0; i < Lenght; i++)
+                {
+                    if (j == 0 || j == Width - 1) Console.Write(symbol);
+                    else if (i == 0 || i == Lenght - 1) Console.Write(symbol);
+                    else Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ hwButton/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hwButton
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Button button = new Button(10,5,3,3);
+
+            button.ReadButton();
+        }
+    }
+}
